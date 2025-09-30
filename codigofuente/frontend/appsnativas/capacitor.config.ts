dabccaf8028a9ccc38cb892583243645971ba1b6
Benjamin <mirandabenjamin615@gmail.com>
2025-09-30 16:44:29 -0300

    Proyecto ionic

---
+++ codigofuente/frontend/appsnativas/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appsnativas',
+  webDir: 'www'
+};
+
+export default config;
